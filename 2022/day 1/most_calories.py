66f76c875c45136d05d0a40c1ed8f63cb87da63b
Parampreet Singh <76559816+Param302@users.noreply.github.com>
2022-12-05 00:28:38 +0530

    Refractored functions

--- 2022/day 1/most_calories.py
+++ 2022/day 1/most_calories.py
@@ -4,49 +4,39 @@ Date - 4/12/22
 Purpose - Solving day1 advent of code.
 """
 
-def highest_calories() -> str:
+# reading input data
+with open("./inputs/day1.txt") as f:
+    data = f.readlines()
 
-    with open("./inputs/day1.txt") as f:
-        data = f.readlines()
-        calory_list = []
-        calories = 0
-        for index, i in enumerate(data):
-            if i == "\n":
+
+def count_calories(data:list[str]) -> list[int]:
+    
+    calory_list = []
+    calories = 0
+    for index, i in enumerate(data):
+        if i == "\n":
+            calory_list.append(calories)
+            calories = 0
+        else:
+            if index == len(data) - 1:
+                calories += int(i)
                 calory_list.append(calories)
-                calories = 0
             else:
-                if index == len(data) - 1:
-                    calories += int(i)
-                    calory_list.append(calories)
-                else:
-                    calories += int(i)
+                calories += int(i)
     calory_list.sort(reverse=True)
+    
+    return calory_list
 
-    return calory_list[0]
 
-def top_three_highest() -> str:
+def highest_calories(calories: list[int]) -> int:
+    return calories[0]
+
+def top_three_highest(calories: list[int]) -> int:
     """Returns the sum of top three highest Elves"""
-
-    with open("./inputs/day1.txt") as f:
-        data = f.readlines()
-
-        calory_list = []
-        calories = 0
-        for index, i in enumerate(data):
-            if i == "\n":
-                calory_list.append(calories)
-                calories = 0
-            else:
-                if index == len(data) - 1:
-                    calories += int(i)
-                    calory_list.append(calories)
-                else:
-                    calories += int(i)
-
-    calory_list.sort(reverse=True)
-    return calory_list[0] + calory_list[1] + calory_list [2]
+    return sum(calories[:3])
 
 if __name__ == "__main__":
-    result1 = highest_calories()
-    result2 = top_three_highest()
+    calories = count_calories(data)
+    result1 = highest_calories(calories)
+    result2 = top_three_highest(calories)
     print(result1, result2)
